acba63dd23dc00da329ab272d0450b48b51a5b10
nnm <roman.syrchin@gmail.com>
2018-05-26 13:07:04 +0500

    Observed Observable - the first approach.

--- app/src/main/java/ru/geekbrains/android3_1/model/CounterModel.java
+++ app/src/main/java/ru/geekbrains/android3_1/model/CounterModel.java
@@ -1,23 +1,46 @@
 package ru.geekbrains.android3_1.model;
 
+import android.icu.util.TimeUnit;
+
 import java.util.ArrayList;
 import java.util.List;
+
+import io.reactivex.Observable;
+import io.reactivex.Observer;
+import io.reactivex.functions.Function;
+
+
 
 public class CounterModel
 {
     List<Integer> counters;
 
+    Observable<Integer> cc;
+
     public CounterModel()
     {
+
         counters = new ArrayList<>();
         counters.add(0);
         counters.add(0);
         counters.add(0);
     }
 
-    public int calculate(int index){
+
+    public Observable<Integer> calculate(int index) {
         int value = counters.get(index) + 1;
         counters.set(index, value);
-        return value;
+        return new Observable<Integer>() {
+            @Override
+            protected void subscribeActual(Observer observer) {
+                try {
+                    java.util.concurrent.TimeUnit.MILLISECONDS.sleep(2000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                observer.onNext(value);
+            }
+        };
     }
+
 }
--- app/src/main/java/ru/geekbrains/android3_1/presenter/MainPresenter.java
+++ app/src/main/java/ru/geekbrains/android3_1/presenter/MainPresenter.java
@@ -3,8 +3,20 @@ package ru.geekbrains.android3_1.presenter;
 import com.arellomobile.mvp.InjectViewState;
 import com.arellomobile.mvp.MvpPresenter;
 
+import io.reactivex.Observable;
+import io.reactivex.Observer;
+import io.reactivex.Scheduler;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Action;
+import io.reactivex.functions.Consumer;
+import io.reactivex.functions.Function;
+import io.reactivex.internal.schedulers.ComputationScheduler;
+import io.reactivex.schedulers.Schedulers;
+import kotlin.jvm.functions.Function0;
 import ru.geekbrains.android3_1.model.CounterModel;
 import ru.geekbrains.android3_1.view.MainView;
+import timber.log.Timber;
 
 @InjectViewState
 public class MainPresenter extends MvpPresenter<MainView>
@@ -13,6 +25,7 @@ public class MainPresenter extends MvpPresenter<MainView>
 
     public MainPresenter(CounterModel model)
     {
+        Timber.i("MainPresenter");
         this.model = model;
     }
 
@@ -24,16 +37,24 @@ public class MainPresenter extends MvpPresenter<MainView>
 
     public void buttonClick(MainView.Buttons id)
     {
-        final int value = model.calculate(id.ordinal());
         switch (id){
             case ONE:
-                getViewState().setButtonOneText(value + "");
+                model.calculate(id.ordinal())
+                        .subscribeOn(Schedulers.computation())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribe( (n) -> getViewState().setButtonOneText(n.toString()));
                 break;
             case TWO:
-                getViewState().setButtonTwoText(value + "");
+                model.calculate(id.ordinal())
+                        .subscribeOn(Schedulers.computation())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribe( (n) -> getViewState().setButtonTwoText(n.toString()));
                 break;
             case THREE:
-                getViewState().setButtonThreeText(value + "");
+                model.calculate(id.ordinal())
+                        .subscribeOn(Schedulers.computation())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribe( (n) -> getViewState().setButtonThreeText(n.toString()));
                 break;
         }
     }
